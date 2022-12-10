5bd9de6e70897e35fc0478825397825b119d819c
lucysileo <64927052+lucysileo@users.noreply.github.com>
2022-12-09 22:35:35 -0500

    Update and rename tokens to tokens.java

---
+++ tokens.java
@@ -0,0 +1,18 @@
+//String + String does concatenation
+string.Concat(string);
+//String * Natural repeats the Natural: 
+string.repeat(nat);
+//Assign bool to natural is allowed
+nat term[] = "bool";
+//Assign natural to bool is allowed
+bool term[] = "nat";
+//Assign char to natural is allowed
+char term[] = "nat";
+//Assign natural to char is allowed 
+nat term[] = "char";
+//Assign natural to real is allowed 
+nat term[] = "real";
+//Dividing by zero is an error:
+if (expression = 0) return "Error";
+Modulo operating by zero
+if(expression = 0) return "Error";
